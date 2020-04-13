a534dc8d2f72da2ebf3d92ce76cf433461db00f5
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-04-13 16:54:10 +0530

    Updates

---
+++ precache-manifest.d9b8e0d9d90eae61f5f4a9de6b5282ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cca538b8ec96276c5382ae528534318",
+    "url": "/weather-react-app/index.html"
+  },
+  {
+    "revision": "21a7915e6325a3af3cb4",
+    "url": "/weather-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "159bfe6ad56b16265be1",
+    "url": "/weather-react-app/static/js/2.47d01763.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react-app/static/js/2.47d01763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a7915e6325a3af3cb4",
+    "url": "/weather-react-app/static/js/main.bda741f9.chunk.js"
+  },
+  {
+    "revision": "83f5f25959ad4ad404db",
+    "url": "/weather-react-app/static/js/runtime-main.a474dfc9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
